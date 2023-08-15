d7ad4551aaac9380cfc7c46361a0b393d9e67d12
Stephen Solka <stephen@solka.dev>
2023-08-15 12:44:30 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Readability.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:floki, "~> 0.21.0"},
+      {:floki, "~> 0.21"},
       {:httpoison, "~> 1.5"},
       {:ex_doc, "~> 0.14", only: :dev},
       {:credo, "~> 0.9", only: [:dev, :test]},
